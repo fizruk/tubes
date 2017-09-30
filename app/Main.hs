5b297603a063db9933d64c7a8958275ca08872f2
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2017-09-30 22:24:45 +0300

    Init gloss with a black screen

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import Graphics.Gloss.Interface.Pure.Game
 
 main :: IO ()
-main = someFunc
+main =
+  play display bgColor fps initialWorld renderWorld handleWorld updateWorld
+  where
+    display = InWindow "Tubes" winSize winOffset
+    bgColor = black
+    fps     = 60
+
+    initialWorld = ()
+    renderWorld w = blank
+    handleWorld _ w = w
+    updateWorld _ w = w
+
+    winSize = (800, 450)
+    winOffset = (100, 100)
+
